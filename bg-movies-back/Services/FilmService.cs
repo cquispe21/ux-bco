2a1b99d145a6ccadb3799d0d48504d4672ffe6ba
crisdaavid-dev <cquispe@intuitosa.com>
2024-12-20 16:46:19 -0500

    Agregar archivos de proyecto.

---
+++ bg-movies-back/Services/FilmService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using bg_movies.Interfaces;
+using System;
+using bg_movies.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace bg_movies.Services
+{
+    public class FilmService : IFilmService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+
+        public FilmService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        public async Task<string> GetFilmCatalogAsync()
+        {
+            string apiKey = _configuration["TheMovieDB:ApiKey"];
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var response = await client.GetAsync($"https://api.themoviedb.org/3/movie/popular?api_key={apiKey}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
